4ca8176868bf385c77cf383fd4c40b658f6a3371
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-04 12:52:20 +0200

    refactor(AddNewUser): change styles to more suitable

--- src/pages/AddNewUser/AddNewUser.jsx
+++ src/pages/AddNewUser/AddNewUser.jsx
@@ -8,7 +8,6 @@ import Profile from "../../components/Profile";
 import { makeStyles, withStyles } from "@material-ui/core/styles";
 import Typography from "@material-ui/core/Typography";
 import Container from "@material-ui/core/Container";
-import Paper from "@material-ui/core/Paper";
 import Tabs from "@material-ui/core/Tabs";
 import Grid from "@material-ui/core/Grid";
 import Tab from "@material-ui/core/Tab";
@@ -41,8 +40,14 @@ function a11yProps(index) {
 const StyledTab = withStyles(theme => ({
   root: {
     textTransform: "none",
-    minWidth: 72,
+
     background: "#EAF1FD",
+
+    fontFamily: "Roboto",
+    fontStyle: "normal",
+    fontWeight: "bold",
+    lineHeight: "28px",
+    fontSize: "24px",
 
     "&$selected": {
       color: "white",
@@ -83,23 +88,21 @@ export default function AddNewUser() {
         Adding new user
       </Grid>
 
-      <Paper>
-        <Tabs
-          classes={{ indicator: classes.tabIncticator }}
-          aria-label="Registration"
-          onChange={handleChange}
-          variant="fullWidth"
-          value={value}
-        >
-          <StyledTab label="1. Account" {...a11yProps(0)} />
+      <Tabs
+        classes={{ indicator: classes.tabIncticator }}
+        aria-label="Registration"
+        onChange={handleChange}
+        variant="fullWidth"
+        value={value}
+      >
+        <StyledTab label="1. Account" {...a11yProps(0)} />
 
-          <StyledTab label="2. Profile" {...a11yProps(1)} />
+        <StyledTab label="2. Profile" {...a11yProps(1)} />
 
-          <StyledTab label="3. Contacts" {...a11yProps(2)} />
+        <StyledTab label="3. Contacts" {...a11yProps(2)} />
 
-          <StyledTab label="4. Capabilities" {...a11yProps(3)} />
-        </Tabs>
-      </Paper>
+        <StyledTab label="4. Capabilities" {...a11yProps(3)} />
+      </Tabs>
 
       <TabPanel value={value} index={0}>
         <Account />
@@ -119,65 +122,3 @@ export default function AddNewUser() {
     </Container>
   );
 }
-
-// import React, { useState } from "react";
-
-// import Capabilities from "../../components/Capabilities";
-// import Contacts from "../../components/Contacts";
-// import Account from "../../components/Account";
-// import Profile from "../../components/Profile";
-
-// import { makeStyles } from "@material-ui/core/styles";
-// import Container from "@material-ui/core/Container";
-// import Grid from "@material-ui/core/Grid";
-// import Tabs from "@material-ui/core/Tabs";
-// import Tab from "@material-ui/core/Tab";
-
-// const useStyles = makeStyles(theme => ({
-//   container: {},
-
-// heading: {
-//   padding: "3rem 0",
-
-//   fontFamily: "Roboto",
-//   fontStyle: "normal",
-//   fontWeight: "bold",
-//   lineHeight: "41px",
-//   fontSize: "35px"
-// }
-// }));
-
-// function AddNewUser() {
-//   const classes = useStyles();
-
-//   const [value, setValue] = useState(0);
-
-//   const handleChange = (event, newValue) => {
-//     setValue(newValue);
-//   };
-
-//   const handleChange
-
-//   return (
-// <Container maxWidth="md">
-//   <Grid className={classes.heading} container justify="center">
-//     Adding new user
-//   </Grid>
-
-//       <Tabs
-//         value={value}
-//         indicatorColor="primary"
-//         textColor="primary"
-//         onChange={handleChange}
-//         aria-label="disabled tabs example"
-//       >
-//         <Capabilities />
-//         <Contacts />
-//         <Account />
-//         <Profile />
-//       </Tabs>
-//     </Container>
-//   );
-// }
-
-// export default AddNewUser;
