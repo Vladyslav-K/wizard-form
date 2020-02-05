2f45bc4373a0ce9250a2a27e64ad8ab58f933475
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-05 18:40:33 +0200

    Updates

---
+++ precache-manifest.5002a73ee3c27e2125f783daf7d44149.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc21c61497a939395eb4b2e6d3108583",
+    "url": "/wizard-form/index.html"
+  },
+  {
+    "revision": "c76ee27afdd277204afd",
+    "url": "/wizard-form/static/css/main.cf96ccdd.chunk.css"
+  },
+  {
+    "revision": "3c0244a7a25e2f1c1266",
+    "url": "/wizard-form/static/js/2.1e24425d.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/wizard-form/static/js/2.1e24425d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c76ee27afdd277204afd",
+    "url": "/wizard-form/static/js/main.10836c6e.chunk.js"
+  },
+  {
+    "revision": "269186651428275abd2c",
+    "url": "/wizard-form/static/js/runtime-main.a8d095fe.js"
+  },
+  {
+    "revision": "2551f83e1b7e1d156dd8d5392c89e697",
+    "url": "/wizard-form/static/media/Logo.2551f83e.svg"
+  },
+  {
+    "revision": "7e9d623db05c8aa46ec4b34453653574",
+    "url": "/wizard-form/static/media/add-new-user.7e9d623d.svg"
+  },
+  {
+    "revision": "0e7d318b3a3018014635f4f7316d4532",
+    "url": "/wizard-form/static/media/add.0e7d318b.svg"
+  },
+  {
+    "revision": "c1387b06e575ea9a67575989be6a8d47",
+    "url": "/wizard-form/static/media/avatar.c1387b06.svg"
+  },
+  {
+    "revision": "9033858740102d27855a2e695b42293a",
+    "url": "/wizard-form/static/media/icon-visibility-off.90338587.svg"
+  },
+  {
+    "revision": "73bb72e44f21d245ea7c7b867daa71cd",
+    "url": "/wizard-form/static/media/icon-visibility.73bb72e4.svg"
+  },
+  {
+    "revision": "f41d10ed448a258f32b9a9aac4e683ba",
+    "url": "/wizard-form/static/media/list-of-users.f41d10ed.svg"
+  }
+]);
