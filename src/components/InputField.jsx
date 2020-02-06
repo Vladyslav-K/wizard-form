a3daaba40c7121f84c098d4cb11ef7c98d5eb2a7
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-06 15:29:00 +0200

    feat(InputField): implement flexible onChange method

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -4,9 +4,14 @@ import { Field } from "formik";
 import OutlinedInput from "@material-ui/core/OutlinedInput";
 import FormControl from "@material-ui/core/FormControl";
 
-export default function InputField({ label, name }) {
+export default function InputField({ label, name, onChange }) {
   return (
-    <FormControl margin="normal" variant="outlined" size="small">
+    <FormControl
+      onChange={onChange}
+      variant="outlined"
+      margin="normal"
+      size="small"
+    >
       <span> {label} </span>
       <Field name={name} type="input" as={OutlinedInput} />
     </FormControl>
