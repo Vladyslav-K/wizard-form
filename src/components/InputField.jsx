671ef899dfe5ae919aa598900a0c4d735bf84541
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-07 15:48:13 +0200

    fix: styles fix

--- src/components/InputField.jsx
+++ src/components/InputField.jsx
@@ -4,16 +4,17 @@ import { Field } from "formik";
 import OutlinedInput from "@material-ui/core/OutlinedInput";
 import FormControl from "@material-ui/core/FormControl";
 
-export default function InputField({ label, name, onChange }) {
+export default function InputField({ label, name, value, onChange }) {
   return (
-    <FormControl
-      onChange={onChange}
-      variant="outlined"
-      margin="normal"
-      size="small"
-    >
+    <FormControl onChange={onChange} variant="outlined" size="small">
       <span> {label} </span>
-      <Field name={name} type="input" as={OutlinedInput} />
+      <Field
+        style={{ marginBottom: "1.5rem" }}
+        as={OutlinedInput}
+        value={value}
+        type="input"
+        name={name}
+      />
     </FormControl>
   );
 }
--- src/pages/common/AccountForm.jsx
+++ src/pages/common/AccountForm.jsx
@@ -7,11 +7,10 @@ import Grid from "@material-ui/core/Grid";
 
 function AccountForm() {
   return (
-    <Grid container justify="space-around">
+    <Grid container justify="space-around" style={{ marginTop: "2rem" }}>
       <Grid item xs={3}>
         <UserAvatar />
       </Grid>
-
       <Grid item xs={6}>
         <LoginAndPassword />
       </Grid>
