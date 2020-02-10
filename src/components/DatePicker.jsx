5e6892b8c7e435d9abf9d8a56a9a9b93725bbd4a
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-10 21:03:27 +0200

    feat: create DatePicker component

---
+++ src/components/DatePicker.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import ReactDatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+import { ReactComponent as CalendarIcon } from "../images/icons/calendar.svg";
+
+import { makeStyles } from "@material-ui/core/styles";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import OutlinedInput from "@material-ui/core/OutlinedInput";
+import FormControl from "@material-ui/core/FormControl";
+import IconButton from "@material-ui/core/IconButton";
+import Grid from "@material-ui/core/Grid";
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    "& span": {
+      fontFamily: "Roboto",
+      fontStyle: "normal",
+      fontWeight: "normal",
+      fontSize: "14px",
+      lineHeight: "16px",
+      color: "#657C9A"
+    }
+  }
+}));
+
+const DatePicker = ({ birthDate, setProfileData }) => {
+  const classes = useStyles();
+
+  const handleButtonMouseDown = event => {
+    event.preventDefault();
+  };
+
+  const CustomInput = ({ onClick }) => (
+    <FormControl
+      className={classes.container}
+      variant="outlined"
+      margin="normal"
+      size="small"
+    >
+      <Grid container justify="space-between">
+        <span> Birth date </span>
+        <span> * </span>
+      </Grid>
+
+      <OutlinedInput
+        placeholder="DD/MM/YYYY"
+        value={birthDate}
+        endAdornment={
+          <InputAdornment position="end">
+            <IconButton onMouseDown={handleButtonMouseDown} onClick={onClick}>
+              <CalendarIcon />
+            </IconButton>
+          </InputAdornment>
+        }
+      />
+    </FormControl>
+  );
+
+  const CustomInputRef = React.forwardRef((props, ref) => (
+    <CustomInput innerRef={ref} {...props} />
+  ));
+
+  return (
+    <ReactDatePicker
+      onChange={date =>
+        setProfileData({
+          birthDate: date.toLocaleDateString().replace(/\./g, "/")
+        })
+      }
+      customInput={<CustomInputRef />}
+    />
+  );
+};
+
+export default DatePicker;
