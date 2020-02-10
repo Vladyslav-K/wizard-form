9bc3205f21823e5d59c75f21249ab949de20f46e
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-10 21:24:04 +0200

    feat: add ProfileForm content

--- src/pages/common/ProfileForm.jsx
+++ src/pages/common/ProfileForm.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+import ProfileLeftContent from "../../components/ProfileFormComponents/ProfileLeftContent";
+import ProfileRightContent from "../../components/ProfileFormComponents/ProfileRightContent";
+
+import Grid from "@material-ui/core/Grid";
+
 function ProfileForm() {
-  return <div>Profile</div>;
+  return (
+    <Grid container justify="space-around" style={{ marginTop: "2rem" }}>
+      <Grid item xs={4}>
+        <ProfileLeftContent />
+      </Grid>
+      <Grid item xs={4}>
+        <ProfileRightContent />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default ProfileForm;
