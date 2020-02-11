425123a7bf3bd63ead6ec70ed586e2746b7c3914
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-11 15:13:51 +0200

    fix: validations rules

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,15 +1,9 @@
 import * as Yup from "yup";
 
 export const loginAndPasswordValidationSchema = Yup.object().shape({
-  username: Yup.string()
-    .min(2, "User name must be at least 2 characters!")
-    .max(10, "User name must be 10 characters or less!")
-    .required("User name is required!"),
+  username: Yup.string().required("User name is required!"),
 
-  password: Yup.string()
-    .min(6, "Password must be at least 6 characters!")
-    .max(20, "Password must be 10 characters or less!")
-    .required("Password is required!"),
+  password: Yup.string().required("Password is required!"),
 
   passwordConfirmation: Yup.string()
     .oneOf([Yup.ref("password")], "Passwords are not the same!")
