07bba3830b819283a0689804f38d617b13f4a658
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-11 14:29:17 +0200

    feat(database): create store for profile data

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,7 +3,10 @@ import Dexie from "dexie";
 const database = new Dexie("UsersData");
 
 database.version(1).stores({
-  accountData: "++id, username, password, passwordConfirmation, avatar"
+  accountData: "++id, username, password, passwordConfirmation, avatar",
+
+  profileData:
+    "++id, firstName,  birthDate,  lastName,  address,  gender,  email"
 });
 
 export default database;
