b65ad2380b452d9f16dc9c7bf681f168d4423631
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-12 18:20:05 +0200

    feat(sagas): create sagas for adding contacts data to database

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -2,6 +2,7 @@ import { all, takeLatest, select } from "redux-saga/effects";
 
 import { syncDatabaseWithAccountData } from "../domain/accountFormDomain/accountFormActions";
 import { syncDatabaseWithProfileData } from "../domain/profileFormDomain/profileFormActions";
+import { syncDatabaseWithContactsData } from "../domain/contactsFormDomain/contactsFormActions";
 import database from "../utils/database";
 
 function* addAccountDataToDatabase() {
@@ -16,9 +17,16 @@ function* addProfileDataToDatabase() {
   database.profileData.put({ ...profileData, id: 1 });
 }
 
+function* addContactsDataToDatabase() {
+  const contactsData = yield select(state => state.contacts);
+
+  database.contactsData.put({ ...contactsData, id: 1 });
+}
+
 export default function* rootSaga() {
   yield all([
     takeLatest(syncDatabaseWithAccountData.type, addAccountDataToDatabase),
-    takeLatest(syncDatabaseWithProfileData.type, addProfileDataToDatabase)
+    takeLatest(syncDatabaseWithProfileData.type, addProfileDataToDatabase),
+    takeLatest(syncDatabaseWithContactsData.type, addContactsDataToDatabase)
   ]);
 }
