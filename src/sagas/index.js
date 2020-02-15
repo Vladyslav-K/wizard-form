1332a0f0a082c7f400baf1df38298784d0a53a38
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-15 20:09:43 +0200

    fix: temporarily remove sagas

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,32 +1,5 @@
-import { all, takeLatest, select } from "redux-saga/effects";
-
-import { syncDatabaseWithAccountData } from "../domain/accountFormDomain/accountFormActions";
-import { syncDatabaseWithProfileData } from "../domain/profileFormDomain/profileFormActions";
-import { syncDatabaseWithContactsData } from "../domain/contactsFormDomain/contactsFormActions";
-import database from "../utils/database";
-
-function* addAccountDataToDatabase() {
-  const accountData = yield select(state => state.account);
-
-  database.accountData.put({ ...accountData, id: 1 });
-}
-
-function* addProfileDataToDatabase() {
-  const profileData = yield select(state => state.profile);
-
-  database.profileData.put({ ...profileData, id: 1 });
-}
-
-function* addContactsDataToDatabase() {
-  const contactsData = yield select(state => state.contacts);
-
-  database.contactsData.put({ ...contactsData, id: 1 });
-}
+import { all, call } from "redux-saga/effects";
 
 export default function* rootSaga() {
-  yield all([
-    takeLatest(syncDatabaseWithAccountData.type, addAccountDataToDatabase),
-    takeLatest(syncDatabaseWithProfileData.type, addProfileDataToDatabase),
-    takeLatest(syncDatabaseWithContactsData.type, addContactsDataToDatabase)
-  ]);
+  yield all([call(() => console.log("Now there is no saga"))]);
 }
