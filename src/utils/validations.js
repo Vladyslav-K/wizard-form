be860b400fbf31c6cb8ec9cd90c40d9891d86da0
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-16 00:04:36 +0200

    feat(validations): add avatar validation to image size and type

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,11 +1,23 @@
 import * as Yup from "yup";
 
-export const loginAndPasswordValidationSchema = Yup.object().shape({
+const SUPPORTED_FORMATS = ["image/jpg", "image/jpeg", "image/gif", "image/png"];
+
+export const AccountFormValidationSchema = Yup.object().shape({
   username: Yup.string().required("User name is required!"),
 
   password: Yup.string().required("Password is required!"),
 
   passwordConfirmation: Yup.string()
     .oneOf([Yup.ref("password")], "Passwords are not the same!")
-    .required("Password confirmation is required!")
+    .required("Password confirmation is required!"),
+
+  avatar: Yup.mixed()
+    .test(
+      "fileSize",
+      "Image must be 1 MB or less!",
+      image => image.size <= 1000000
+    )
+    .test("fileType", "Unsupported File Format", image =>
+      SUPPORTED_FORMATS.includes(image.type)
+    )
 });
