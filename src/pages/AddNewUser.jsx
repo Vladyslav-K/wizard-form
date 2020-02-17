fee61fab2a38b1dc72dbc165882c000b66230a45
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-17 22:15:36 +0200

    feat(AddNewUser): implement query string navigation for tabs

--- src/pages/AddNewUser.jsx
+++ src/pages/AddNewUser.jsx
@@ -1,6 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
-import CapabilitiesForm from "./common/CapabilitiesForm";
+import CapabilitiesFormContainer from "./common/CapabilitiesForm/CapabilitiesFormContainer";
 import ContactsFormContainer from "./common/ContactsForm/ContactsFormContainer";
 import AccountFormContainer from "./common/AccountForm/AccountFormContainer";
 import ProfileFormContainer from "./common/ProfileForm/ProfileFormContainer";
@@ -12,6 +12,63 @@ import Tabs from "@material-ui/core/Tabs";
 import Grid from "@material-ui/core/Grid";
 import Tab from "@material-ui/core/Tab";
 import Box from "@material-ui/core/Box";
+
+const AddNewUser = ({ history, location }) => {
+  const classes = useStyles();
+
+  const [value, setValue] = useState(0);
+
+  useEffect(() => {
+    const search = location.search;
+    const params = new URLSearchParams(search);
+    const queryIndex = params.get("step");
+
+    setValue(Number(queryIndex) || 0);
+  }, [location.search]);
+
+  const handleChange = (event, value) => {
+    history.push({ search: `?step=${value}` });
+  };
+
+  return (
+    <Container maxWidth="md">
+      <Grid className={classes.heading} container justify="center">
+        Adding new user
+      </Grid>
+      <Tabs
+        classes={{ indicator: classes.tabIncticator }}
+        aria-label="Registration"
+        onChange={handleChange}
+        variant="fullWidth"
+        value={value}
+      >
+        <StyledTab label="1. Account" {...a11yProps(0)} />
+
+        <StyledTab label="2. Profile" {...a11yProps(1)} />
+
+        <StyledTab label="3. Contacts" {...a11yProps(2)} />
+
+        <StyledTab label="4. Capabilities" {...a11yProps(3)} />
+      </Tabs>
+
+      <TabPanel value={value} index={0}>
+        <AccountFormContainer />
+      </TabPanel>
+
+      <TabPanel value={value} index={1}>
+        <ProfileFormContainer />
+      </TabPanel>
+
+      <TabPanel value={value} index={2}>
+        <ContactsFormContainer />
+      </TabPanel>
+
+      <TabPanel value={value} index={3}>
+        <CapabilitiesFormContainer />
+      </TabPanel>
+    </Container>
+  );
+};
 
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
@@ -50,6 +107,10 @@ const StyledTab = withStyles(theme => ({
     fontWeight: "bold",
     lineHeight: "28px",
     fontSize: "24px",
+
+    "& span": {
+      userSelect: "none"
+    },
 
     "&$selected": {
       color: "white",
@@ -103,54 +164,5 @@ const useStyles = makeStyles(theme => ({
     display: "none"
   }
 }));
-
-const AddNewUser = () => {
-  const classes = useStyles();
-
-  const [value, setValue] = useState(0);
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <Container maxWidth="md">
-      <Grid className={classes.heading} container justify="center">
-        Adding new user
-      </Grid>
-      <Tabs
-        classes={{ indicator: classes.tabIncticator }}
-        aria-label="Registration"
-        onChange={handleChange}
-        variant="fullWidth"
-        value={value}
-      >
-        <StyledTab label="1. Account" {...a11yProps(0)} />
-
-        <StyledTab label="2. Profile" {...a11yProps(1)} />
-
-        <StyledTab label="3. Contacts" {...a11yProps(2)} />
-
-        <StyledTab label="4. Capabilities" {...a11yProps(3)} />
-      </Tabs>
-
-      <TabPanel value={value} index={0}>
-        <AccountFormContainer />
-      </TabPanel>
-
-      <TabPanel value={value} index={1}>
-        <ProfileFormContainer />
-      </TabPanel>
-
-      <TabPanel value={value} index={2}>
-        <ContactsFormContainer />
-      </TabPanel>
-
-      <TabPanel value={value} index={3}>
-        <CapabilitiesForm />
-      </TabPanel>
-    </Container>
-  );
-};
 
 export default AddNewUser;
