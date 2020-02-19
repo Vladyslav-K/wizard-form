8c501b22d2e70ac8e3e0ea3248630dd1e3e35c39
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-19 12:10:04 +0200

    feat(sagas): implement sync database and redux store

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,5 +1,83 @@
-import { all, call } from "redux-saga/effects";
+import { all, call, put, takeLatest } from "redux-saga/effects";
+
+import database from "../utils/database.js";
+
+import {
+  syncCapabilitiesDataWithDatabase,
+  setCapabilitiesData
+} from "../domain/capabilitiesFormDomain/capabilitiesFormActions.js";
+
+import {
+  syncContactsDataWithDatabase,
+  setContactsData
+} from "../domain/contactsFormDomain/contactsFormActions.js";
+
+import {
+  syncProfileDataWithDatabase,
+  setProfileData
+} from "../domain/profileFormDomain/profileFormActions.js";
+
+import {
+  syncAccountDataWithDatabase,
+  setAccountData
+} from "../domain/accountFormDomain/accountFormActions.js";
+
+const TEMPORARY_USER_KEYPATH = 1;
+
+function* getUserDataFromDatabase() {
+  const data = yield call(
+    {
+      context: database.temporaryUserData,
+      fn: database.temporaryUserData.get
+    },
+    TEMPORARY_USER_KEYPATH
+  );
+
+  return data;
+}
+
+export function* syncReduxStoreWithDatabase() {
+  const dataWithDatabase = yield call(() => getUserDataFromDatabase());
+
+  if (dataWithDatabase) {
+    yield all([
+      put(syncCapabilitiesDataWithDatabase(dataWithDatabase)),
+      put(syncContactsDataWithDatabase(dataWithDatabase)),
+      put(syncProfileDataWithDatabase(dataWithDatabase)),
+      put(syncAccountDataWithDatabase(dataWithDatabase))
+    ]);
+  }
+}
+
+export function* syncDatabaseWithRedux(action) {
+  if (action.payload) {
+    const dataWithDatabase = yield call(() => getUserDataFromDatabase());
+
+    yield call(
+      {
+        context: database.temporaryUserData,
+        fn: database.temporaryUserData.put
+      },
+      {
+        ...dataWithDatabase,
+        ...action.payload,
+        keyPath: TEMPORARY_USER_KEYPATH
+      }
+    );
+  }
+}
 
 export default function* rootSaga() {
-  yield all([call(() => console.log("Now there is no saga"))]);
+  yield all([
+    call(() => syncReduxStoreWithDatabase()),
+    takeLatest(
+      [
+        setCapabilitiesData.type,
+        setContactsData.type,
+        setProfileData.type,
+        setAccountData.type
+      ],
+      syncDatabaseWithRedux
+    )
+  ]);
 }
