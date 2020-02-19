f41626a0648786ffb392db4063bb2c53926e8848
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-19 11:54:06 +0200

    refactor(database): create table for temporary user data

--- src/utils/database.js
+++ src/utils/database.js
@@ -3,13 +3,7 @@ import Dexie from "dexie";
 const database = new Dexie("UsersData");
 
 database.version(1).stores({
-  accountData: "++id, username, password, passwordConfirmation, avatar",
-
-  profileData:
-    "++id, firstName,  birthDate,  lastName,  address,  gender,  email",
-
-  contactsData:
-    "++id, mainLanguage,  facebookLink,  gitHubLink,  company,  phone,  fax"
+  temporaryUserData: "++keyPath, username, email, firstName, lastName"
 });
 
 export default database;
