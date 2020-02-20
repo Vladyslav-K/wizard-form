9beb97ea51baf7477a5ecdec56b52c73af39a06a
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-02-20 23:33:29 +0200

    feat(ListOfUsers): create common sagas methods for a list of users
    Implement sagas for sending, reading and synchronizing
    a list of users from the database.

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,16 +1,32 @@
 import { all, call, put, select, takeLatest } from "redux-saga/effects";
+import { DateTime } from "luxon";
 
 import database from "../utils/database.js";
 
 import {
   syncTemporaryUserDataWithDatabase,
+  removeTemporaryUserData,
   setTemporaryUserData
 } from "../domain/temporaryUserDomain/temporaryUserActions.js";
 
+import {
+  syncUserListWithDatabase,
+  userListFetchingError,
+  removeUserFromList,
+  userListIsLoading,
+  addUserToList
+} from "../domain/userListDomain/userListActions.js";
+
 const TEMPORARY_USER_KEYPATH = 1;
 
-function* getUserDataFromDatabase() {
-  const data = yield call(
+function* getUserListFromDatabase() {
+  const userList = yield call(() => database.userList.toArray());
+
+  return userList;
+}
+
+function* getTemporaryUserDataFromDatabase() {
+  const temporaryUserData = yield call(
     {
       context: database.temporaryUserData,
       fn: database.temporaryUserData.get
@@ -18,11 +34,21 @@ function* getUserDataFromDatabase() {
     TEMPORARY_USER_KEYPATH
   );
 
-  return data;
+  return temporaryUserData;
 }
 
-export function* syncReduxStoreWithDatabase() {
-  const dataWithDatabase = yield call(() => getUserDataFromDatabase());
+function* removeTemporaryUser() {
+  yield call(
+    {
+      context: database.temporaryUserData,
+      fn: database.temporaryUserData.delete
+    },
+    TEMPORARY_USER_KEYPATH
+  );
+}
+
+export function* syncReduxTemporaryUserDataWithDatabase() {
+  const dataWithDatabase = yield call(() => getTemporaryUserDataFromDatabase());
 
   const dataWithRedux = yield select(state => state.temporaryUserData);
 
@@ -38,8 +64,8 @@ export function* syncReduxStoreWithDatabase() {
   }
 }
 
-export function* syncDatabaseWithRedux(action) {
-  const dataWithDatabase = yield call(() => getUserDataFromDatabase());
+export function* syncDatabaseTemporaryUserDataWithRedux(action) {
+  const dataWithDatabase = yield call(() => getTemporaryUserDataFromDatabase());
 
   yield call(
     {
@@ -54,9 +80,58 @@ export function* syncDatabaseWithRedux(action) {
   );
 }
 
+export function* syncReduxUserListWithDatabase() {
+  yield put(userListIsLoading());
+
+  try {
+    const userListWithDB = yield call(() => getUserListFromDatabase());
+    yield put(syncUserListWithDatabase(userListWithDB));
+  } catch {
+    yield put(userListFetchingError());
+  }
+}
+
+export function* putUserToDatabaseUserList() {
+  const userData = yield select(state => state.temporaryUserData);
+
+  yield call(
+    {
+      context: database.userList,
+      fn: database.userList.add
+    },
+    {
+      ...userData,
+      createdAt: DateTime.local().toJSDate(),
+      updatedAt: DateTime.local().toJSDate()
+    }
+  );
+}
+
+export function* removeUserFromDatabaseUserList(action) {
+  yield call(
+    {
+      context: database.userList,
+      fn: database.userList.delete
+    },
+    action.payload.id
+  );
+}
+
 export default function* rootSaga() {
   yield all([
-    call(() => syncReduxStoreWithDatabase()),
-    takeLatest(setTemporaryUserData.type, syncDatabaseWithRedux)
+    call(() => syncReduxTemporaryUserDataWithDatabase()),
+
+    call(() => syncReduxUserListWithDatabase()),
+
+    takeLatest(removeTemporaryUserData.type, removeTemporaryUser),
+
+    takeLatest(
+      setTemporaryUserData.type,
+      syncDatabaseTemporaryUserDataWithRedux
+    ),
+
+    takeLatest(addUserToList.type, putUserToDatabaseUserList),
+
+    takeLatest(removeUserFromList.type, removeUserFromDatabaseUserList)
   ]);
 }
