bd56b8701e820ee555f15059982dffdc39bdda4e
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-03-06 18:23:52 +0200

    Updates

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ if ("function" === typeof importScripts) {
     console.log("Workbox is loaded");
 
     /* injection point for manifest files.  */
-    workbox.precaching.precacheAndRoute([{"revision":"82d1b98561b87d67ea4c00cae71c8491","url":"index.html"},{"revision":"3dd13c36e9604f996a8a68327b412498","url":"precache-manifest.3dd13c36e9604f996a8a68327b412498.js"},{"revision":"e3d8be523298b2c4487658e76c469314","url":"service-worker.js"},{"revision":"571048d67ecbabe789e0737b59098ce3","url":"static/css/2.2fcdc29a.chunk.css"},{"revision":"54367887dde9cc5769cfd0063fafc26c","url":"static/css/main.cf96ccdd.chunk.css"},{"revision":"6b2b8fde107c31ab3516f0742896bf28","url":"static/js/2.631932ed.chunk.js"},{"revision":"d75924c8bcb09a69f13d43f95a10bacc","url":"static/js/main.4dc231cc.chunk.js"},{"revision":"737c82dda7363aa6e8cd329a64d26070","url":"static/js/runtime-main.a8d095fe.js"}]);
+    workbox.precaching.precacheAndRoute([{"revision":"ccffe4bf7a315fd0ef369273730425c9","url":"index.html"},{"revision":"4376b790a2ccaa4087a443fdebf6985a","url":"precache-manifest.4376b790a2ccaa4087a443fdebf6985a.js"},{"revision":"6b314be14379997020d9d9843516e8c8","url":"service-worker.js"},{"revision":"571048d67ecbabe789e0737b59098ce3","url":"static/css/2.2fcdc29a.chunk.css"},{"revision":"54367887dde9cc5769cfd0063fafc26c","url":"static/css/main.cf96ccdd.chunk.css"},{"revision":"6b2b8fde107c31ab3516f0742896bf28","url":"static/js/2.631932ed.chunk.js"},{"revision":"8ada61d7d090dfabf9231e42f74ecd93","url":"static/js/main.4dadc247.chunk.js"},{"revision":"737c82dda7363aa6e8cd329a64d26070","url":"static/js/runtime-main.a8d095fe.js"}]);
 
     workbox.core.skipWaiting();
 
