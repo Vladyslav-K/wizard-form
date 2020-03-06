42dc0bf48e6a8db660cc1da8dcc9cf692e8863b1
Vladyslav-K <vladyslav.kliuiev@gmail.com>
2020-03-06 15:54:43 +0200

    Updates

--- sw.js
+++ sw.js
@@ -9,6 +9,8 @@ if ("function" === typeof importScripts) {
     /* injection point for manifest files.  */
     workbox.precaching.precacheAndRoute([{"revision":"47e5b5378ee1619bed7c23d8b905efea","url":"index.html"},{"revision":"3074aedcb0e39ef5cb9e1f22caf0dab7","url":"precache-manifest.3074aedcb0e39ef5cb9e1f22caf0dab7.js"},{"revision":"44c836aaab456b4477744265ac577c30","url":"service-worker.js"},{"revision":"571048d67ecbabe789e0737b59098ce3","url":"static/css/2.2fcdc29a.chunk.css"},{"revision":"54367887dde9cc5769cfd0063fafc26c","url":"static/css/main.cf96ccdd.chunk.css"},{"revision":"53eb895f8abb2075c89a1867309d0bfc","url":"static/js/2.1d3c63a5.chunk.js"},{"revision":"c8359c23a1b222107f21f807fe8148eb","url":"static/js/main.f15a5882.chunk.js"},{"revision":"737c82dda7363aa6e8cd329a64d26070","url":"static/js/runtime-main.a8d095fe.js"}]);
 
+    workbox.core.skipWaiting();
+
     /* custom cache rules*/
     workbox.routing.registerNavigationRoute("/index.html", {
       blacklist: [/^\/_/, /\/[^\/]+\.[^\/]+$/]
@@ -16,7 +18,7 @@ if ("function" === typeof importScripts) {
 
     workbox.routing.registerRoute(
       /\.(?:png|gif|jpg|jpeg)$/,
-      workbox.strategies.networkFirst({
+      workbox.strategies.cacheFirst({
         cacheName: "images",
         plugins: [
           new workbox.expiration.Plugin({
